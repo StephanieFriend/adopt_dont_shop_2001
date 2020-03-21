097314f4942bc1d9c6030735299b7b7fa27408c3
Stephanie Friend <stephaniefriend91@gmail.com>
2020-03-21 16:26:52 -0600

    Complete user story 10 with passing feature tests

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -24,4 +24,27 @@ class PetsController < ApplicationController
   def show
     @pet = Pet.find(params[:id])
   end
+
+  def edit
+    @pet = Pet.find(params[:id])
+  end
+
+  def update
+    pet = Pet.find(params[:id])
+    pet.update({
+                   image: params[:image],
+                   name: params[:name],
+                   description: params[:description],
+                   approximate_age: params[:approximate_age],
+                   sex: params[:sex],
+                   adoptable: params[:adoptable]
+               })
+    pet.save
+    redirect_to "/pets/#{pet.id}"
+  end
+
+  def destroy
+    Pet.destroy(params[:id])
+    redirect_to '/pets'
+  end
 end
